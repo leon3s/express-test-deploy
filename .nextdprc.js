82d8a423e60168b14f9b07ae2b33f5aa7fc92aa0
leon3s <lvernisse@student.42.fr>
2021-11-15 07:34:35 +0100

    test webhook

--- .nextdprc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  commands: [
-    ['npm', ['install']],
-    ['npm', ['start']],
-  ]
-}
--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const express = require('express');
-
 const app = express();
-
 const port = +(process.env.PORT || 8932);
 
 app.get('/', (req, res) => {
-  console.log(req.headers);
-  res.send(JSON.stringify({
+  const response = JSON.stringify({
     name: "express-test-deploy",
     uptime: process.uptime(),
     headers: req.headers,
     env: process.env,
-  }));
+  });
+  console.log(response);
+  res.send(response);
 });
 
 app.listen(port, () => {
-  console.log(`express-test-dp listening on ${port}`);
+  console.log(`express-test-deploy listening on ${port}`);
 });
