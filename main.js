da8f26c14622b7f2f43675969e8faa76f4484c7f
leon3s <lvernisse@student.42.fr>
2021-11-15 11:31:42 +0100

    Added more stylized readme.md serve

--- main.js
+++ main.js
@@ -6,7 +6,29 @@ const port = +(process.env.PORT || 8932);
 
 app.get('/README.md', (req, res) => {
   const buff = fs.readFileSync(path.join(__dirname, 'README.md'));
-  res.send(buff.toString());
+res.send(`
+  <html>
+  <body style="margin:0px;padding:0px;height:100%;width:100%;background-color:black;">
+  <div style="width:50%;height:50%;margin:auto;padding:18px;">
+    ${buff.toString().split('\n').reduce((acc, line) => {
+      const char = line.charAt(0);
+      const balise = 'span';
+      let fontSize = 12;
+      if (char === '#') {
+        fontSize = 16;
+      }
+      acc += `<${balise} style="color: white; font-size:${fontSize}px">`
+      acc += line;
+      acc += `</${balise}>`
+      acc += '</br>'
+      return acc;
+    }, '')}
+  </div>
+  <p style="color:white;font-size:10px;">
+  </p>
+  </body>
+  </html>
+`);
 })
 
 app.get('/', (req, res) => {
